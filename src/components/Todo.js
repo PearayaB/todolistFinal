e33bc856ce88ad917231829ce09e53b060493f31
PearayaB <84061338+PearayaB@users.noreply.github.com>
2022-03-28 18:46:09 +0700

    edit value

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -17,7 +17,7 @@ const Todo = ({ todo, setTodo, setEdit }) => {
         return (
           <div className="customers" key={ todo._id }>
             <div
-              className={todo.isComplete ? "todo-row complete" : "todo-row"}
+              className={todo ? "todo-row complete" : "todo-row"}
               key={index}
             >
               <div >
@@ -46,36 +46,7 @@ const Todo = ({ todo, setTodo, setEdit }) => {
       })}
     </>
   );
-  // return todos.map((todo, index) => (
-  //   <div className="customers">
 
-  //     <div 
-  //       className={todo.isComplete ? "todo-row complete" : "todo-row"}
-  //       key={index}
-  //     >
-  //       <div >
-  //         {todo.priority}
-  //       </div>
-  //       <div key={todo.id} onClick={() => completeTodo(todo.id)}>
-  //         {todo.text}
-  //       </div>
-  //       <div className="icons">
-  //          {todo.date}
-  //          <div className="checkbox">
-  //         <Checkbox />
-  //         </div>
-  //         <TiEdit
-  //           onClick={() => {setEdit({ id: todo.id, value: todo.text ,priority:todo.priority,date:todo.date});}}
-  //           className="edit-icon" 
-  //         />
-  //         <RiCloseCircleLine
-  //           onClick={() => removeTodo(todo.id)}
-  //           className="delete-icon"
-  //         />
-  //       </div>
-  //     </div>
-  //   </div>
-  // ));
 };
 
 export default Todo;
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -7,6 +7,7 @@ function TodoForm({ todo, setTodo, edit, setEdit }) {
   const [inputMessage, setInputMessage] = useState('');
   const [date, setDate] = useState('');
 
+
   const addTodo = () => {
     const newTodo = { _id: todo.length + 1, message: inputMessage, priority: priority, date: date };
     setTodo([newTodo, ...todo].sort((a, b) => {
@@ -23,7 +24,8 @@ function TodoForm({ todo, setTodo, edit, setEdit }) {
   const handleSubmit = (e) => {
     e.preventDefault();
     addTodo();
-    reset();
+    reset()
+    
   };
 
   const handleSubmitEdit = (e) => {
@@ -31,10 +33,15 @@ function TodoForm({ todo, setTodo, edit, setEdit }) {
     const editTodo = { _id: todo[edit.index]._id, message: inputMessage, priority: priority, date: date };
     todo[edit.index] = editTodo;
     setTodo([...todo]);
+    setPriority(todo[edit.index].priority);
     setEdit({ status: false, index: undefined });
-    reset();
-  }
+   
 
+    console.log(editTodo)
+  }
+// const myDate =moment().format('MM/DD/YYYY')
+const today = moment(new Date().getDate(),'DD/MM/YYYY')
+// console.log(today)
   return (
     <>
       {!edit.status ? (
@@ -42,6 +49,7 @@ function TodoForm({ todo, setTodo, edit, setEdit }) {
           <InputNumber
             onChange={priority => setPriority(priority)}
             value={priority}
+            placeholder="priority"
           />
           <input
             className="todo-input"
@@ -50,20 +58,30 @@ function TodoForm({ todo, setTodo, edit, setEdit }) {
             value={inputMessage}
           />
           <DatePicker onChange={(_, dateString) => setDate(dateString)} value={date ? moment(date) : null} />
-          <button type="submit" className="todo-button">Add new todo</button>
+          <button type="submit" className="todo-button">Add to do</button>
         </form>
       ) : (
         <form onSubmit={handleSubmitEdit}>
           <InputNumber
-            placeholder={todo[edit.index].priority}
+            // placeholder={todo[edit.index].priority}
+            value={todo[edit.index].priority}
             onChange={priority => setPriority(priority)}
+            
           />
           <input
             className="todo-input"
-            placeholder={todo[edit.index].message}
+            // placeholder={todo[edit.index].message}
+            value={todo[edit.index].message}
             onChange={message => setInputMessage(message.target.value)}
+           
           />
-          <DatePicker onChange={(_, dateString) => setDate(dateString)} placeholder={moment(todo[edit.index].date).format('YYYY-MM-DD')} />
+          
+          <DatePicker 
+           value={today}
+          onChange={(_, dateString) => setDate(dateString)} 
+          // placeholder={moment(todo[edit.index].date).format('DD/MM/YYYY')}
+         
+          />
           <button type="submit" className="todo-button">Edit</button>
         </form>
       )}
